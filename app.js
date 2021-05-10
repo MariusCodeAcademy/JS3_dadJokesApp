6c3ea24c0579472e6656aa7dea58f4f7f1d14e92
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-10 11:40:01 +0300

    veikiaIkiFetch

--- app.js
+++ app.js
@@ -8,6 +8,40 @@ const dummyJokePunch = "You boil the hell out of it";
 // 2  pasirasom css
 
 // 3 isitraukiam reikalingus el i js
+const el = {
+  cat: document.querySelector(".cat"),
+  sentence: document.querySelector(".sentence"),
+  punchline: document.querySelector(".punchline"),
+  punchBtn: document.getElementById("punch"),
+  nextJoke: document.getElementById("nextJoke"),
+};
+
+el.punchBtn.addEventListener("click", punchLineHandler);
+el.nextJoke.addEventListener("click", nextJokeHandler);
+
+function punchLineHandler() {
+  console.log("punch was pressed");
+  showPunchLine();
+}
+
+function nextJokeHandler() {
+  // loadNextJoke()
+  hidePunchLine();
+}
+
+function showPunchLine() {
+  el.punchline.classList.add("open");
+}
+function hidePunchLine() {
+  el.punchline.classList.remove("open");
+}
+
+function init() {
+  el.sentence.innerHTML = dummyJokeLine;
+  el.punchline.innerHTML = dummyJokePunch;
+}
+
+init();
 
 // 4 paadarom kad uzkrovus psl mes matom savo klausima ir mygtukus
 // paspaudus mygutka "Punch" pasirodo buves pasleptas punchline sakinys
