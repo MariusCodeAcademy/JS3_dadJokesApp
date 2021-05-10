e71e202828ba222e1856f0f289f71b0ab5b74a6f
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-10 10:43:30 +0300

    init

---
+++ app.js
@@ -0,0 +1,15 @@
+"use strict"; // here we go again
+
+const dummyJokeLine = "How do you make holy water?";
+const dummyJokePunch = "You boil the hell out of it";
+
+// 1 susikurti html musu app
+
+// 2  pasirasom css
+
+// 3 isitraukiam reikalingus el i js
+
+// 4 paadarom kad uzkrovus psl mes matom savo klausima ir mygtukus
+// paspaudus mygutka "Punch" pasirodo buves pasleptas punchline sakinys
+
+// 5 paspaudus mygtuka "Next Joke" is naujo matosi tik klausiamasis sakinys;
